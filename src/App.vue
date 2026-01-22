9ac2997ee7c211cdad938a6a6bb7a196dafc3644
Jeambo <226494145+jeambos@users.noreply.github.com>
2026-01-22 12:11:26 +0800

    取消调试主题色时增加的强制使用亮色主题的代码

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,6 @@
 import AppHeader from './components/AppHeader.vue'; // 引入组件
 import { onMounted } from 'vue';
 
-// --- 强制注入主题 ---
-onMounted(() => {
-  // 强制给 html 标签加上 data-theme="light"
-  document.documentElement.setAttribute('data-theme', 'light');
-  console.log('主题已强制设置为 light (浅色)');
-});
 
 </script>
 
